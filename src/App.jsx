55e12acd4e29c2ed88f3a760072d450611053b0b
dodisutarya <ikhwands@protonmail.ch>
2025-11-02 06:45:21 +0700

    styling components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 const Card = ({ title }) => {
   return (
-    <div><h2>{title}</h2></div>
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
   )
 }
 
 const App = () => {
   return (
-    <div>
-      <h2>Functional Arrow Component</h2>
-      <Card title="Star Wars" rating={5} isCool={true} actors={[{ name: 'Actors' }]} />
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
       <Card title="Avatar" />
       <Card title="The Lion King" />
     </div>
-
-
   )
 }
 
