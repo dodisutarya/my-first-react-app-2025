c6bb07f5ba38e36e86335ddb8d1f09bd2309ed48
dodisutarya <ikhwands@protonmail.ch>
2025-11-02 09:03:53 +0700

    hooks, useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect, use } from 'react'
+
 const Card = ({ title }) => {
+
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`The card "${title}" has been ${hasLiked ? 'liked' : 'unliked'}.`);
+  }, [hasLiked]);
+
+  useEffect(() => {
+    console.log('CARD RENDERED');
+  }, []);
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)} >
+      <h2>{title} <br /> {count || null}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)} >
+        {hasLiked ? '❤️' : '🤍'}
+
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
+
+
   return (
     <div className="card-container">
       <Card title="Star Wars" rating={5} isCool={true} />
