3d1a2c056002288d281c8533ee4e0e15319a82ec
dodisutarya <ikhwands@protonmail.ch>
2025-11-02 21:32:08 +0700

    movie card development

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { use } from 'react'
 import { useEffect, useState } from 'react'
 import Search from './components/Search'
 import Spinner from './components/Spinner'
+import MovieCard from './components/MovieCard';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -70,7 +71,7 @@ const App = () => {
         <h1 className='text-white'>{searchTerm}</h1>
 
         <section className='all-movies'>
-          <h2 className='mt-[40px]'>All Movies</h2>
+          <h2 className='mt-10'>All Movies</h2>
 
           {isLoading ? (
             <Spinner />
@@ -79,7 +80,7 @@ const App = () => {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p key={movie.id} className='text-white' >{movie.title}</p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
